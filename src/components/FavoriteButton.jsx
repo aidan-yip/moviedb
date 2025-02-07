2c680622915a9e62700dce32130aafa96797720f
Muskan <muskanbhatia3010@gmail.com>
2025-02-07 13:49:40 -0800

    added favbutton

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,4 +1,18 @@
-function FavoriteButton ({movie}){
-    const {favorites, addFavorite,removeFavorite} = useContext(FavoritesContext);
+import { GlobalContext } from "../context/GlobalContext";
 
+function FavoriteButton ({movie}){
+    const {favorites, addFavorite,removeFavorite} = useContext(GlobalContext);
+    const isFavorite = favorites.find((fav) => {
+        return fav.id === movie.id;
+    });
+    function handleClick(event){
+        event.stopPropagation();
+        if(isFavorite){
+            removeFavorite(movie.id);
+        } else {
+            addFavorite(movie);
+        }
+    }
+    return <button onClick={handleClick}>{isFavorite ? "Remove from Favorites" : "Add to Favorites"}</button>;
 }
+export default FavoriteButton;
